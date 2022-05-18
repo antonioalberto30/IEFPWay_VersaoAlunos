70a73f298c34bb2bdfcc9c03b0fa84540cfe9167
antonioanberto30 <2721558@formacao.iefp.pt>
2022-05-18 12:27:15 +0100

    alteracao   ate hoje

--- database/connectDatabase.php
+++ database/connectDatabase.php
@@ -1,15 +1,15 @@
 <?php
 
-
-
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "";
+$dbname = "iefpway";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
--- pages/login.php
+++ pages/login.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
- * Página de login - Deve permitir ir para o index.php
- */
+
 require("../database/connectDatabase.php");
-
-session_start();
-
-if (isset($_SESSION["id"])) {
-   // header("Location: /pages/topup.php");
-}
 
 ?>
 <!DOCTYPE html>
@@ -39,20 +31,17 @@ if (isset($_SESSION["id"])) {
 
     <!-- Favicon and touch icons -->
     <link rel="shortcut icon" href="/pages/assets/ico/favicon.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144"
-          href="/pages/assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114"
-          href="/pages/assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/pages/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/pages/assets/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/pages/assets/ico/apple-touch-icon-72-precomposed.png">
     <link rel="apple-touch-icon-precomposed" href="/pages/assets/ico/apple-touch-icon-57-precomposed.png">
+    <style>
 
+        .inlineimage{max-width:470px;margin-right: 8px;margin-left: 10px}.images{display: inline-block;max-width: 98%;height: auto;width: 22%;margin: 1%;left:20px;text-align: center}
+    </style>
 </head>
 
 <body>
-
-
-
-
 
 <!-- Top content -->
 <div class="top-content">
@@ -61,7 +50,7 @@ if (isset($_SESSION["id"])) {
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-sm-offset-2 text">
-                    <h1><strong>IEFPWay</strong> Login</h1>
+                    <h1><strong>IEFPWay</strong> Register</h1>
                     <div class="description">
                     </div>
                 </div>
@@ -70,78 +59,133 @@ if (isset($_SESSION["id"])) {
                 <div class="col-sm-6 col-sm-offset-3 form-box">
                     <div class="form-top">
                         <div class="form-top-left">
-                            <h3>Login to our site</h3>
-                            <p>Enter your username and password to log on:</p>
+                            <h3>Register on our site</h3>
+                            <p>Send money to your friends</p>
                         </div>
                         <div class="form-top-right">
                             <i class="fa fa-lock"></i>
                         </div>
                     </div>
 
+
                     <?php
 
-                    if (isset($_POST["form-username"]) && isset($_POST["form-password"])) {
+                    //VERIFICA TODOS OS CAMPOS
+
+                    if( isset($_POST["form-username"]) && isset($_POST["form-email"]) && isset($_POST["form-contact"])
+                        && isset($_POST["form-password"]) && isset($_POST["form-confirm-password"])
+                        && isset($_POST["form-card-number"])  && isset($_POST["form-card-name"])
+                        && isset($_POST["form-card-csv"])  && isset($_POST["form-card-expiration"])
+                    ){
 
 
-                        // COMPLETAR ... PROCURAR NA BASE DE DADOS -> POR UM USER COM O EMAIL E PASSWORD COLOCADOS NO FORM
-                        // COLOCAR NA VARIAVEL O RESULTADO DA BASE DE DADOS
+                        //COMPLETAR -> Dá erro caso a password não seja igual a Confirmação da password  MOSTRAR A MSG
 
-                        $result = null;
+                        /*
+                            <div class="alert alert-warning" role="alert">
+                                The password and confirm password didn't match
+                            </div>
+                        */
+                        ?>
+                        <?php
 
-                        //ERRO NA BASE DE DADOS
+                        //Query para inserir o cartao e obter o id desse cartao  AAAA
+                        $sql_card = $conn->prepare("INSERT INTO Card(name,csv,card_code,expirationDate) VALUES (?,?,?,?);");
+                        $sql_card->bind_param("sdss", $_POST["form-card-name"], $_POST["form-card-csv"], $_POST["form-card-number"],$_POST["form-card-expiration"]);
+                        $sql_card->execute();
+                        $cardId = $conn -> insert_id;
+
+                        //COMPLETAR INSERIR O UTILIZADOR COM O CARD ID
+                        $sql_user = $conn->prepare("INSERT INTO user(name,email,contact,password, card_id) VALUES (?,?,?,?,?);");
+                        $sql_user->bind_param("sssss", $_POST["form-username"], $_POST["form-email"], $_POST["form-contact"],$_POST["form-password"], $cardId);
+                        $sql_user->execute();
+
+
+                        //Se der erro dá esta mensagem
                         if (mysqli_error($conn)) {
                             ?>
-
                             <div class="alert alert-danger" role="alert">
-                                User or password invalid
+                                Error creating user
                             </div>
-
-                        <?php
-
-                        //RESULTADO DA BASE DE DADOS > 0 ENTAO EXIST
-                        } else if (mysqli_num_rows($result) > 0) {
-
-
-                        //GUARDAMOS NA SESSAO O ID DO UTILIZADOR
-                        $_SESSION["id"] = mysqli_fetch_assoc($result)["id"];
-                        ?>
+                            <?php
+                        }else{
+                            //se não der erro dá esta mensagem
+                            ?>
                             <div class="alert alert-success" role="alert">
-                                Sucess Redirecting
-                            </div>
-                            <script>
-                                document.location.reload(); //SC   Recarrega a página,   Vai dar refresh na página
-                            </script>
-                        <?php
-
-                        } else {
-                        ?>
-                            <div class="alert alert-danger" role="alert">
-                                Error password and user combination invalid
+                                You are now registered
                             </div>
                             <?php
                         }
+
                     }
 
                     ?>
 
 
                     <div class="form-bottom">
-                        <form role="form" method="post" class="login-form">
+                        <form role="form"  method="post" class="login-form">
                             <div class="form-group">
                                 <label class="sr-only" for="form-username">Username</label>
-                                <input type="text" name="form-username" placeholder="Username..."
-                                       class="form-username form-control" id="form-username">
+                                <input type="text" name="form-username" placeholder="Username..." class="form-username form-control" id="form-username" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-email">Email</label>
+                                <input type="text" name="form-email" placeholder="Email..." class="form-username form-control" id="form-email" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-contact">Contacto</label>
+                                <input type="text"   name="form-contact" placeholder="Contact..." class="form-username form-control" id="form-contact" required>
                             </div>
                             <div class="form-group">
                                 <label class="sr-only" for="form-password">Password</label>
-                                <input type="password" name="form-password" placeholder="Password..."
-                                       class="form-password form-control" id="form-password">
+                                <input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password" required>
                             </div>
-                            <button type="submit" class="btn">Sign in!</button>
+                            <div class="form-group">
+                                <label class="sr-only" for="form-password2">Confirm password</label>
+                                <input type="password" name="form-confirm-password" placeholder="Confirm password..." class="form-password form-control" id="form-password2" required>
+                            </div>
 
+
+                            <div class="">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <div class="row">
+                                            <h3 class="text-center">Payment Details</h3>
+                                            <div class="inlineimage"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Mastercard-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Discover-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Paypal-Curved.png"> <img class="img-responsive images" src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/American-Express-Curved.png"> </div>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-xs-12">
+                                                <div class="form-group"> <label>CARD NUMBER</label>
+                                                    <div class="input-group"> <input type="text" class="form-control" placeholder="Valid Card Number" name="form-card-number" required /> <span class="input-group-addon"><span class="fa fa-credit-card"></span></span> </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-7 col-md-7">
+                                                <div class="form-group"> <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label> <input type="text" class="form-control" name="form-card-expiration" placeholder="MM / YY" required/> </div>
+                                            </div>
+                                            <div class="col-xs-5 col-md-5 pull-right">
+                                                <div class="form-group"> <label>CV CODE</label> <input type="text" class="form-control" placeholder="CVC"  name="form-card-csv" required/> </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-12">
+                                                <div class="form-group"> <label>CARD OWNER</label> <input type="text" class="form-control" placeholder="Card Owner Name"  name="form-card-name"  required/> </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+
+                            <button type="submit" class="btn">Sign up!</button>
                         </form>
-                        <a class="btn" style="background-color: #5cb85c" href="/pages/register.php"
-                        >Sign up!</a>
+                        <a class="btn"  href="/pages/login.php"
+                        >Sign in!</a>
                     </div>
                 </div>
             </div>
